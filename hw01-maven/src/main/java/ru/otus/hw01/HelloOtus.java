882806af15b0111e437825d932afbc7b32ee3f7b
Artem Eltyshev <elty@yandex-team.ru>
2019-06-30 01:00:13 +0300

    init maven prj, add hw01-maven module

---
+++ hw01-maven/src/main/java/ru/otus/hw01/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.otus.hw01;
+
+import com.google.common.base.Strings;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(Strings.repeat("Hello, world!\n", 10));
+    }
+}
